e567e9286e0284ec0a2ce7f1bca77906232b8586
maaz siddiqui <moaaz7151@gmail.com>
2022-10-07 23:16:39 +0500

    Updates

---
+++ precache-manifest.cba01e1b0ea6ac7c224f42196785ac5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1def3369e98099f5fdc4",
+    "url": "/PIAIC-Homepage-Clone/static/css/main.01cdeefc.chunk.css"
+  },
+  {
+    "revision": "1def3369e98099f5fdc4",
+    "url": "/PIAIC-Homepage-Clone/static/js/main.1def3369.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/PIAIC-Homepage-Clone/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "2422e8cf59ffd808e10f",
+    "url": "/PIAIC-Homepage-Clone/static/css/2.34cacefd.chunk.css"
+  },
+  {
+    "revision": "2422e8cf59ffd808e10f",
+    "url": "/PIAIC-Homepage-Clone/static/js/2.2422e8cf.chunk.js"
+  },
+  {
+    "revision": "15617e11f4b2fab1df15",
+    "url": "/PIAIC-Homepage-Clone/static/js/runtime~main.15617e11.js"
+  },
+  {
+    "revision": "117117c7866f64029cae41279dbfea58",
+    "url": "/PIAIC-Homepage-Clone/index.html"
+  }
+];
